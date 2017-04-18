d2745bd9854c8208f5d60f69b742ad2b6a142874
marcoverzero <marcoverzero@gmail.com>
2017-04-18 09:48:27 -0600

    pantry tracks multiple restocks

--- lib/pantry.rb
+++ lib/pantry.rb
@@ -1,3 +1,15 @@
 class Pantry
 
+  attr_reader :stock
+  def initialize
+    @stock = Hash.new(0)
+  end
+
+  def stock_check(item)
+    stock[item]    #### downcase item?
+  end
+
+  def restock(item, quantity)
+    stock[item] = stock[item] += quantity
+  end
 end
--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -2,8 +2,39 @@ require './lib/pantry'
 require 'minitest/autorun'
 require 'minitest/pride'
 
-#lookit this confirmation comment
-
 class PantryTest < Minitest::Test
+  def test_it_exists
+    pantry = Pantry.new
+
+    assert_instance_of Pantry, pantry
+  end
+
+  def test_it_has_stock
+    pantry = Pantry.new
+
+    assert_equal ({}), pantry.stock
+  end
+
+  def test_it_can_check_stock
+    pantry = Pantry.new
+
+    assert_equal 0, pantry.stock_check("Cheese")
+  end
+
+  def test_it_can_restock
+    pantry = Pantry.new
+    pantry.restock("Cheese", 10)
+
+    assert_equal 10, pantry.stock_check("Cheese")
+  end
+
+  def test_it_can_track_multiple_restocks
+    pantry = Pantry.new
+    pantry.restock("Cheese", 10)
+    pantry.restock("Cheese", 20)
+
+    assert_equal 30, pantry.stock_check("Cheese")
+  end
+
 
 end
